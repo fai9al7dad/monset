c8614bbcaeaf2b40f26ef935022db6021ca32c35
faisal-haddad <139450826+faisal-haddad@users.noreply.github.com>
2024-05-22 03:49:32 +0300

    Initial Commit

---
+++ monset/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  monset
+//
+//  Created by faisal haddad on 14/11/1445 AH.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ monset/monsetApp.swift
@@ -0,0 +1,17 @@
+//
+//  monsetApp.swift
+//  monset
+//
+//  Created by faisal haddad on 14/11/1445 AH.
+//
+
+import SwiftUI
+
+@main
+struct monsetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
